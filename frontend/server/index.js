81cd4597cdbe835925b34b779d56c273ef93641e
shaikumar0 <23A91A0560@aec.edu.in>
2026-02-09 19:23:00 +0530

    final things

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -44,6 +44,12 @@ if (fs.existsSync(staticPath)) {
     }
 }
 
+// Log all incoming requests
+app.use((req, res, next) => {
+    console.log(`[REQUEST] ${req.method} ${req.url}`);
+    next();
+});
+
 // Serve static files from the Angular app
 app.use(express.static(staticPath));
 
